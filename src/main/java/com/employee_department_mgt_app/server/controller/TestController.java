3ed525addec1e288904003554ed4b2392479811d
Sachini Buddhika <sachiniste3@gmail.com>
2025-02-26 12:15:51 +0530

    added swagger dependencies

---
+++ src/main/java/com/employee_department_mgt_app/server/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.employee_department_mgt_app.server.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("")
+    public String test() {
+        return "test";
+    }
+}
