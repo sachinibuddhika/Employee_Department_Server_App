9658ea22176587ce583d7a21be892ccc22001ec5
Sachini Buddhika <sachiniste3@gmail.com>
2025-02-26 07:10:35 +0530

    first commit

---
+++ src/main/java/com/employee_department_mgt_app/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.employee_department_mgt_app.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/employee_department_mgt_app/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employee_department_mgt_app.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
